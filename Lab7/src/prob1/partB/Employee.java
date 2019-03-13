a8854dd6d54d9062a9b287bc1b928d7a9a446660
enku2015 <enku.wende@gmail.com>
2019-03-12 21:14:24 -0500

    Lab7 Prob1 Submission

---
+++ Lab7/src/prob1/partB/Employee.java
@@ -0,0 +1,35 @@
+package prob1.partB;
+
+public class Employee {
+	private String name;
+	private int salary;
+	public Employee(String name, int salary) {
+		this.name = name;
+		this.salary = salary;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getSalary() {
+		return salary;
+	}
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+	@Override 
+	public String toString() {
+		return "(" + name + ", " + salary + ")";
+	}
+
+	@Override
+	public boolean equals(Object e) {
+		if(e == null) return false;
+		if(e.getClass() != this.getClass()) return false;
+		Employee emp = (Employee) e;
+		return emp.name.equals(this.name) && emp.salary == this.salary;
+	}
+}
