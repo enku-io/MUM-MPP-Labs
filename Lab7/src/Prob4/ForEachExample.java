cae4ac505195da63781d73fcab535716717edbe6
enku2015 <enku.wende@gmail.com>
2019-03-13 19:32:06 -0500

    Lab7 Prob4

---
+++ Lab7/src/Prob4/ForEachExample.java
@@ -0,0 +1,34 @@
+package Prob4;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Locale;
+import java.util.TreeSet;
+import java.util.function.Consumer;
+
+
+public class ForEachExample {
+	@SuppressWarnings("unused")
+	public static void main(String[] args) {
+		List<String> list = Arrays.asList("Hello there", "Goodbye", "Back soon", 
+				"Away", "On Vacation", "Everywhere you want to be");
+
+		//print each element of the list in upper case format
+		Consumer<String> consumer = new Consumer<String>(){
+
+			@Override
+			public void accept(String s) {
+				System.out.println(s.toUpperCase());
+			}
+		};
+
+		list.forEach(consumer);
+		
+		
+	}
+	
+	//implement a Consumer
+
+
+
+}
